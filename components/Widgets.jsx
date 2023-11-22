27b7308671d6ee66b332de45d118368c774c67fe
T0mer007 <tomerbenaim1@gmail.com>
2023-11-22 16:50:30 +0200

    create the randomUsers section of the Widgets component

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -4,8 +4,9 @@ import News from './News';
 import { useState } from 'react';
 
 
-export default function Widgets({ news }) {
+export default function Widgets({ news, users }) {
     const [articleNum, setArticleNum] = useState(3)
+    const [randUserNum, setRandUserNum] = useState(3)
     return (
         <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
             <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
@@ -28,6 +29,22 @@ export default function Widgets({ news }) {
                 </button>
             </div>
 
+                    <div className='sticky top-16 text-gray-700 space-y-3 bg-gray-100 pt-2 rounded-xl w-[90%] xl:w-[75%]'>
+                        <h4 className='font-bold text-xl px-4'>Who to follow</h4>
+                        {users.slice(0,randUserNum).map((randUser)=> (
+                            <div key={randUser.login.username} className='flex items-center px-4 py-2 cursor-pointer hover:bg-gray-200 '>
+                                <img src={randUser.picture.thumbnail} width={40} alt="user image" className='rounded-full' />
+                                <div className="truncate ml-4 leading-5">
+                                <h4 className='font-bold hover:underline text-[14px] truncate'>{randUser.login.username}</h4>
+                                <h5 className='text-[13px] truncate text-gray-500'>{randUser.name.first}+ +{randUser.name.last}</h5>
+                                </div>
+                                <button className='ml-auto bg-black text-white rounded-full text-sm px-3.5 py-1.5 font-bold'>Follow</button>
+                            </div>
+                        ))}
+                        <button onClick={() => setRandUserNum(randUserNum + 3)} className='text-blue-300 pl-4 pb-3 hover:text-blue-400'>Show more</button>
+                    </div>
+
         </div>
     )
 }
+ 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,8 +11,21 @@ export default async function Home() {
 
     return newsResult.json()
   }
+
+  
+  async function getUsers() {
+    const users = await fetch('https://randomuser.me/api/?results=50&inc=name,picture,login')
+    if (!users.ok) {
+      throw new Error('Failed to fetch data')
+    }
+
+    return users.json()
+  }
+
+  
+
   const news = await getData()
-  // console.log(news)
+  const users = await getUsers()
   return (
     <main className='flex min-h-screen mx-auto'>
 
@@ -20,7 +33,7 @@ export default async function Home() {
 
       <Feed />
 
-      <Widgets news={news.articles} />
+      <Widgets news={news.articles} users={users.results} />
 
     </main>
   )
