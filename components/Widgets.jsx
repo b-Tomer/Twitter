8db7056356283635d4c9661699d8479edf4ccc9a
T0mer007 <tomerbenaim1@gmail.com>
2023-11-29 20:10:39 +0200

    deploy bug fixed

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -20,7 +20,7 @@ export default function Widgets({ news, users }) {
                 </div>
             </div>
             <div className='text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] xl:w-[75%]'>
-                <h4 className='font-bold text-xl px-4'>What's happening</h4>
+                <h4 className='font-bold text-xl px-4'>What&lsquo;s happening</h4>
                 {news.slice(0, articleNum).map((article) => (
                     <News key={article.title} article={article} />
                 ))}
